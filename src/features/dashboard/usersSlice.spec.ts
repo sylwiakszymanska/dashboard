9e19ac2ba339ef09ba07ae1c9bdd7a9cb9fa8c76
Sylwia Szymańska <sylwiakszymanska@gmail.com>
2023-01-29 16:37:08 +0100

    Add sorting feature

--- src/features/dashboard/usersSlice.spec.ts
+++ src/features/dashboard/usersSlice.spec.ts
@@ -1,31 +1,12 @@
-import usersReducer, {
+import usersReducer, { UsersState } from "./usersSlice";
 
-} from './usersSlice';
+describe("users reducer", () => {
+  const initialState: UsersState = { usersList: [] };
 
-describe('users reducer', () => {
-  // const initialState: UsersState = {
-  //   value: 3,
-  //   status: 'idle',
-  // };
-  // it('should handle initial state', () => {
-  //   expect(counterReducer(undefined, { type: 'unknown' })).toEqual({
-  //     value: 0,
-  //     status: 'idle',
-  //   });
-  // });
-
-  // it('should handle increment', () => {
-  //   const actual = counterReducer(initialState, increment());
-  //   expect(actual.value).toEqual(4);
-  // });
-
-  // it('should handle decrement', () => {
-  //   const actual = counterReducer(initialState, decrement());
-  //   expect(actual.value).toEqual(2);
-  // });
-
-  // it('should handle incrementByAmount', () => {
-  //   const actual = counterReducer(initialState, incrementByAmount(2));
-  //   expect(actual.value).toEqual(5);
-  // });
+  it("should handle initial state", () => {
+    expect(usersReducer(undefined, { type: "unknown" })).toEqual({
+      value: 0,
+      status: "idle",
+    });
+  });
 });
